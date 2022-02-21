2a080a04ec0d1047a7369ba2aa3ff08b3703b0b3
riya2820 <60728693+riya2820@users.noreply.github.com>
2022-02-20 23:15:39 -0800

    Create minDepthBST.py

---
+++ minDepthBST.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if root == None:
+            return 0
+        elif root.left == None:
+            return 1 + self.minDepth(root.right)
+        elif root.right == None:
+            return 1 + self.minDepth(root.left)
+        else:
+            return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
