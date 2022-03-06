9511ba5d0ebdfddf02e989ba29b0707dd2aa59b6
riya2820 <60728693+riya2820@users.noreply.github.com>
2022-03-05 21:55:47 -0800

    Create isSubsequence.py

---
+++ isSubsequence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        remainder_of_t = iter(t)
+        
+        for letter in s:
+            if letter not in remainder_of_t:
+                return False
+        return True
