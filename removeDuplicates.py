ed522b0ba12dfb600f46d4dd1215510f4fb232e9
Riya Joshi <60728693+riya2820@users.noreply.github.com>
2022-04-12 23:59:07 -0700

    Update removeDuplicates.py

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -4,12 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if (nums.length == 0) return 0;
-    int i = 0;
-    for (int j = 1; j < nums.length; j++) {
-        if (nums[j] != nums[i]) {
-            i++;
-            nums[i] = nums[j];
-        }
-    }
-    return i + 1;
+        if (nums.length == 0):
+            return 0
+        
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != nums[i]):
+                i+= 1
+                nums[i] = nums[j]
+            
+        return i + 1
