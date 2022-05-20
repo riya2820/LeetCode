628dc8253a20277439eb3c506eb1fa699900d390
Riya Joshi <60728693+riya2820@users.noreply.github.com>
2022-05-20 14:03:14 -0700

    Create houseRobber.py

---
+++ houseRobber.py
@@ -0,0 +1,14 @@
+def rob(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    rob1, rob2 = 0, 0
+    
+    for n in nums:
+        temp = max(n + rob1, rob2)
+        rob1 = rob2
+        rob2 = temp
+    return rob2
+
+print(rob([2,1,1,2]))
