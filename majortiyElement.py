cec2e6e537715ed74bde86aef3ccc9852e84b6be
Riya Joshi <60728693+riya2820@users.noreply.github.com>
2022-06-26 17:08:53 -0700

    Update majortiyElement.py

--- majortiyElement.py
+++ majortiyElement.py
@@ -1,11 +1,24 @@
+from collections import defaultdict 
+
 class Solution(object):
     def majorityElement(self, nums):
         """
         :type nums: List[int]
         :rtype: int
-        """        
-        #Brute Force: O(N^2) time complexity
+        """
         #Hash Map Method: O(N) time complexity
-       
-        counts = collections.Counter(nums)
-        return max(counts.keys(), key=counts.get)
+        d = defaultdict(int)
+        majorityElement = 0
+     
+          
+        # or just write, d = Counter(nums)
+        for num in nums:
+            d[num] += 1
+            
+        for i in d:
+            if d[i] > (len(nums)/2):
+                majorityElement = i
+                
+        return majorityElement
+        
+
