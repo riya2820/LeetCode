816aad86686e39711803677660a1ef4102843333
Riya Joshi <60728693+riya2820@users.noreply.github.com>
2022-07-11 18:49:41 -0700

    Update isRobotBounded.py

--- isRobotBounded.py
+++ isRobotBounded.py
@@ -1,22 +1,30 @@
-class Solution:
-    def isRobotBounded(self, instructions: str) -> bool:
-        # north = 0, east = 1, south = 2, west = 3
-        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        # Initial position is in the center
-        x = y = 0
-        # facing north
-        idx = 0
-        
+class Solution(object):
+    def isRobotBounded(self, instructions):
+        """
+        :type instructions: str
+        :rtype: bool
+        """
+        x, y = 0, 0
+        dx, dy = 0, 1
+
         for i in instructions:
-            if i == "L":
-                idx = (idx + 3) % 4
-            elif i == "R":
-                idx = (idx + 1) % 4
-            else:
-                x += directions[idx][0]
-                y += directions[idx][1]
-        
-        # after one cycle:
-        # robot returns into initial position
-        # or robot doesn't face north
-        return (x == 0 and y == 0) or idx != 0
+            if i == 'L':  
+                dx, dy = -dy, dx # Turn left 
+                print("Left: ",dx,dy)
+
+            elif i == 'R' :  
+                dx, dy = dy, -dx # Turn right 
+                print("Right: ",dx,dy)
+
+            elif i == 'G': 
+                x += dx
+                y += dy
+                print("Straight: ",x,y)
+
+        if (x, y) == (0, 0): # Case 1: Robot returns to rigin
+            return True
+
+        if (dx, dy) != (0, 1): # Case 2: Robot does not face north 
+            return True
+
+        return False
