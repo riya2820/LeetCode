6ba2c16499f222bc24d8cbceae5c12bbcf298769
Riya Joshi <60728693+riya2820@users.noreply.github.com>
2022-10-13 18:05:20 -0700

    Create construct2Darray.py

---
+++ construct2Darray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:   
+        if not original:
+            return original 
+        
+        if len(original) != m*n:
+            return []
+  
+        result = []
+        for i in range(0, len(original), n):
+            result.append(original[i:i+n])
+            
+        return result
